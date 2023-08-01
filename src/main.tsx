57953198c04adba20b5aebc73e625222a5e5a0af
nonloso220 <78572202+nonloso220@users.noreply.github.com>
2023-08-01 18:28:19 +0200

    fix: bug translation

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import AppRoutes from './routes/index.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/applicationStore.ts'
-import './i18n.config.ts'
+// import './i18n.config.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
